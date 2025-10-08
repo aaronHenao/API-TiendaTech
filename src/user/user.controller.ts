22e79bd1c10b0fb987962aa3c5cebad4e1f71351
aaronHenao <agiraldo363@soyudemedellin.edu.co>
2025-10-07 21:00:10 -0500

    Se agregaron variables de entorno para la conexión con la db. Se actualizó el service para crear un cliente, un admin y obtener un usuario por email, en el controller se crearon endpoints para crear cliente, crear el primer admin y crear otro admin(se va a segurar despues)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { BaseApplicationResponse } from 'src/common/dto/base-application-response.dto';
+import { UserResponseDto } from './dto/user-response.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){}
+
+    @Post('register')
+    async create(@Body() newUser: CreateUserDto): Promise<BaseApplicationResponse<UserResponseDto>>{
+        const user = await this.userService.create(newUser);
+        return{
+            statusCode: 201, 
+            message: 'Usuario creado correctamente',
+            data: user
+        }
+    }
+
+    //Este endpoint es temporal. Bórralo o coméntalo después de crear el primer admin
+    @Post('create-first-admin')
+    async createFirstAdmin(@Body() admin: CreateUserDto): Promise<BaseApplicationResponse<UserResponseDto>>{
+        const dataAdmin = await this.userService.createAdmin(admin);
+        return{
+            statusCode: 201, 
+            message: 'Administrador creado correctamente',
+            data: dataAdmin
+        }
+    }
+
+    //Despues de crear el primer admin, usa este endpoint protegido
+    @Post('admin')
+    async createNewAdmin(@Body() newAdmin: CreateUserDto): Promise<BaseApplicationResponse<UserResponseDto>>{
+        const admin = await this.userService.createAdmin(newAdmin);
+        return{
+            statusCode: 201, 
+            message: 'Administrador creado correctamente',
+            data: admin
+        }
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Rol, User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserResponseDto } from './dto/user-response.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectRepository(User) private userRespository: Repository<User>){}
+
+    async create(createUser: CreateUserDto): Promise<UserResponseDto>{
+        const existingUser = await this.userRespository.findOneBy({email: createUser.email});
+
+        if(existingUser){
+            throw new BadRequestException('El correo ya está registrado en el sistema')
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(createUser.password, saltRounds);
+        const newUser = this.userRespository.create({...createUser, password: hashedPassword, rol: Rol.CUSTOMER,});
+        const savedUser = await this.userRespository.save(newUser);
+        return new UserResponseDto(savedUser);
+    }
+
+    async createAdmin(createAdmin: CreateUserDto): Promise<UserResponseDto>{
+        const existingEmail = await this.userRespository.findOneBy({email: createAdmin.email});
+
+        if(existingEmail){
+            throw new BadRequestException('El correo ya está registrado en el sistema como un cliente')
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(createAdmin.password, saltRounds);
+        const newAdmin = this.userRespository.create({...createAdmin, password: hashedPassword, rol: Rol.ADMIN,});
+        const savedAdmin = await this.userRespository.save(newAdmin);
+        return new UserResponseDto(savedAdmin);
+    }
+
+    async findOneByEmail(email: string): Promise<UserResponseDto>{
+        const user = await this.userRespository.findOneBy({email})
+        return new UserResponseDto(user)
+    }
+}
