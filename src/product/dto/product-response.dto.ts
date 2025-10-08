aaddc187ccb9898bb4890f614f838e84cd12de6f
aaronHenao <agiraldo363@soyudemedellin.edu.co>
2025-10-08 17:45:06 -0500

    Se edita el ProductResponseDto

--- src/product/dto/product-response.dto.ts
+++ src/product/dto/product-response.dto.ts
@@ -1,10 +1,8 @@
 // product-response.dto.ts
-import { Expose, Type } from "class-transformer";
+import { Expose, Transform, TransformFnParams, Type } from "class-transformer";
 import { CategoryResponseDto } from "src/category/dto/category-response.dto";
 
 export class ProductResponseDto {
-    @Expose()
-    id: number;
     
     @Expose()
     name: string;
@@ -15,12 +13,5 @@ export class ProductResponseDto {
     @Expose()
     @Type(() => Number)
     price: number; 
-
-    @Expose()
-    stock: number;
-    
-    @Expose()
-    @Type(() => CategoryResponseDto) 
-    categories?: CategoryResponseDto[]; 
 
 }
